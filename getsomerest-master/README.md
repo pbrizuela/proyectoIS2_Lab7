7bf812013e800e9fe43914fcfc11d842930874b1
pbrizuela <pedrobri777@gmail.com>
2017-05-21 23:19:44 -0400

    dc
    sdds

---
+++ getsomerest-master/README.md
@@ -0,0 +1,16 @@
+# getsomerest
+
+Example code from my Jersey web service blogs. This branch is tailored to be used with NetBeans 8 and Jersey 2.5.  To get the Jersey 1.8 version checkout the jersey-1.8 branch.
+
+```
+git clone git@github.com:vichargrave/getsomerest.git
+git checkout jersey-1.8
+```
+
+# Build
+
+1. Configure NetBeans and Tomcat according to my blog article.
+2. Start NetBeans.
+3. Select __File > Open Project__ in NetBeans.
+4. Open the __getsomerest__ project.
+4. Right click on the project then select __Run__.
---
+++ getsomerest-master/build/generated-sources/ap-source-output/com/example/Usuarios_.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-21T20:23:18")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> correo;
+    public static volatile SingularAttribute<Usuarios, Integer> id;
+    public static volatile SingularAttribute<Usuarios, String> nombre;
+
+}
